32c5db5e6990a7af78fb85ec16cde7fb511f7f96
azertyuiop <excilys@localhost.localdomain>
2017-11-20 15:28:52 +0100

    add custom error pages

--- src/main/java/controller/ControllerCDB.java
+++ src/main/java/controller/ControllerCDB.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import dto.CompanyDTO;
 import dto.ComputerDTO;
@@ -157,5 +158,6 @@ public class ControllerCDB {
 		service.deleteCompany(selectionCompany);
 		return "redirect:dashboard";
 	}
+	
 	
 }
